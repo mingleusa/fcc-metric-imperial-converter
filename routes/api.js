457010d2182fd62aaadebf880ce42a6e1b046913
mingleusa <admin@jordanmingle.dev>
2026-06-30 16:14:25 -0400

    Add query parameter support for FCC /api/convert tests

--- routes/api.js
+++ routes/api.js
@@ -6,40 +6,50 @@ module.exports = function (app) {
 
   let convertHandler = new ConvertHandler();
 
-  app.get(/^\/api\/convert\/(.+)$/, function (req, res) {
-      const input = req.params[0];
+  function handleConvert(input, res) {
+    let initNum = convertHandler.getNum(input);
+    let initUnit = convertHandler.getUnit(input);
 
-      let initNum = convertHandler.getNum(input);
-      let initUnit = convertHandler.getUnit(input);
-
-      if (initNum === 'invalid number' && initUnit === 'invalid unit') {
-        return res.json({
-          initNum: 'invalid number',
-          initUnit: 'invalid number and unit'
-        });
-      }
-
-      if (initNum === 'invalid number') {
-        return res.json({ initNum: 'invalid number' });
-      }
-
-      if (initUnit === 'invalid unit') {
-        return res.json({ initUnit: 'invalid unit' });
-      }
-
-      const returnUnit = convertHandler.getReturnUnit(initUnit);
-      let returnNum = convertHandler.convert(initNum, initUnit);
-      returnNum = Math.round(returnNum * 100000) / 100000;
-
-      const string = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
-
-      res.json({
-        initNum,
-        initUnit,
-        returnNum,
-        returnUnit,
-        string
+    if (initNum === 'invalid number' && initUnit === 'invalid unit') {
+      return res.json({
+        initNum: 'invalid number',
+        initUnit: 'invalid number and unit'
       });
+    }
+
+    if (initNum === 'invalid number') {
+      return res.json({ initNum: 'invalid number' });
+    }
+
+    if (initUnit === 'invalid unit') {
+      return res.json({ initUnit: 'invalid unit' });
+    }
+
+    const returnUnit = convertHandler.getReturnUnit(initUnit);
+    let returnNum = convertHandler.convert(initNum, initUnit);
+    returnNum = Math.round(returnNum * 100000) / 100000;
+
+    const string = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
+
+    res.json({
+      initNum,
+      initUnit,
+      returnNum,
+      returnUnit,
+      string
+    });
+  }
+
+  app.get('/api/convert', function (req, res) {
+    const input = req.query.input;
+    if (!input) {
+      return res.status(404).type('text').send('Not Found');
+    }
+    handleConvert(input, res);
+  });
+
+  app.get(/^\/api\/convert\/(.+)$/, function (req, res) {
+    handleConvert(req.params[0], res);
   });
 
 };
